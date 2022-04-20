f4fc118bb9d21a7d2fe5ef8477db5af0b50a8379
Ömer Akyol <omer.akyol@gmail.com>
2022-04-20 21:04:24 +0300

    render settings

--- Raytrace/MakeVideo.bat
+++ Raytrace/MakeVideo.bat
@@ -1,2 +1,2 @@
 :: ffmpeg -r 1/5 -i img%03d.png -c:v libx264 -vf fps=25 -pix_fmt yuv420p out.mp4
-ffmpeg -framerate 60 -i "Render/Frame_%%05d.png" -c:v libx264 -pix_fmt yuv420p "RenderVideo/output.mp4"
+ffmpeg -framerate 60 -i "Render/Frame_%%05d.png" -crf 23 -c:v libx264 -pix_fmt yuv420p "RenderVideo/output.mp4"
